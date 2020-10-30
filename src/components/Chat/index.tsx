825aed4a2349d3c9befa017cd13d1099d261a956
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-30 14:05:12 -0300

    Chat component created

---
+++ src/components/Chat/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Chat: React.FC = () => {
+  return (
+    <Container>
+      <h1>Chat</h1>
+    </Container>
+  )
+}
+
+export default Chat
