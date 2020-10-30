d2adafff97d6ba1e4ea7ebc8627bddfbe062b50f
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-30 10:48:46 -0300

    Router file created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import Routes from './routes'
+
 import GlobalStyles from './styles/GlobalStyles'
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Routes />
 
       <GlobalStyles />
     </>
